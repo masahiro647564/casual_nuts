5d4de7ace9dc9f4dcf613802d2ad4f0f514d2c93
masahiro-g <kwoaudis32@gmail.com>
2019-03-19 18:24:02 +0900

    post viewページ作成

--- db/migrate/20190319060508_create_posts.rb
+++ db/migrate/20190319060508_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :description
+      t.string :title, null: false, length: 50
+      t.text :description, null: false, length: 1000
 
       t.timestamps
     end
