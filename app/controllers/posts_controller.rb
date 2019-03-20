d9441019198340ec329591512b86522cd22288db
masahiro-g <kwoaudis32@gmail.com>
2019-03-20 16:28:35 +0900

    コントローラーのリファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -18,15 +19,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "ナッツの情報を更新しました!"
     else
@@ -35,7 +33,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "ナッツを削除しました｡"
   end
@@ -44,5 +41,9 @@ class PostsController < ApplicationController
 
     def post_params
       params.require(:post).permit(:title, :description, :new_image)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 end
