6dba4ff27424d2330842f57959edbe3ab53ccdef
masahiro-g <kwoaudis32@gmail.com>
2019-03-25 11:46:11 +0900

    レイアウト調整, _header調整

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,7 +2,7 @@ module PostsHelper
 
   def show_post_image(post)
     if post.image.attached?
-      image_tag post.image.variant(resize: "300x100"), class: "img-thumbnail"
+      image_tag post.image.variant(resize: "200x200"), class: "img-thumbnail"
     else
       image_tag "no_image.png", class: "img-thumbnail"
     end
@@ -10,9 +10,9 @@ module PostsHelper
 
   def show_post_small_image(post)
     if post&.image&.attached?
-      image_tag post.image.variant(resize: "100×100"), class: "img-thumbnail"
+      image_tag post.image.variant(resize: "200×200"), class: "img-thumbnail"
     else
-      image_tag "no_image.png", class: "img-thumbnail",  width: 50
+      image_tag "no_image.png", class: "img-thumbnail",  width: 60
     end
   end
 end
