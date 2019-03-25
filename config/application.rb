a4eaca930a4be8952697021e1ebf117e37c78bd4
masahiro-g <kwoaudis32@gmail.com>
2019-03-25 21:27:45 +0900

    config/application.rb config/environments/production.rbの修正

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module CasualNuts
     config.time_zone = 'Tokyo' # タイムゾーンもついでに変更
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
